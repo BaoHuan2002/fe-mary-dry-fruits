d0ea2c4fa4af625a8b5f89683d9c86d5cd3027c1
Lam Hoang Khai <heoloveninh2014@gmail.com>
2024-01-09 15:53:03 +0700

    9/1

--- src/components/Layout/Detail/Detail_Item/Detail_Review.js
+++ src/components/Layout/Detail/Detail_Item/Detail_Review.js
@@ -7,7 +7,7 @@ const cx = classNames.bind(styles);
 const DetailReview = ({ star }) => {
     return (
         <div className={cx('detail-review')}>
-            <h4 className={cx('detail-review-heading')}>Customer Reviewer</h4>
+            <h4 className={cx('detail-review-heading')}>Customer Reviewe</h4>
             <h6 className={cx('detail-review-title')}>Over View</h6>
             <div className={cx('detail-review-star')}>
                 <span className={cx('detail-review-amount')}>{star}</span>
